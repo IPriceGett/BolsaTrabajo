41e8ee0171cfec0f4a87f5e27ec0f18ee44df000
Ignacio Price <ignacio.price@gett.cl>
2022-04-01 09:26:50 -0300

    creacion de carpeta de metodos

---
+++ Proyecto/src/main/java/com/mycompany/proyecto/Back/utiles/Utiles.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.proyecto.Back.utiles;
+import com.mycompany.proyecto.Back.clases.*;
+
+/**
+ *
+ * @author Ignacio
+ */
+public class Utiles {
+    
+    public void eliminarPostulante(){}
+    public void agregarPostulante() {}
+    public void eliminarSkill() {}
+    public void eliminarTrabajador() {}
+    public void agregarTrabajador() {}
+
+}
